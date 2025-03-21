1f1143261e0fd345c88fc696e6346b94012062fb
Holden Karau <holden@pigscanfly.ca>
2025-03-21 16:46:04 -0700

    Fix domain linking on payments

--- fighthealthinsurance/views.py
+++ fighthealthinsurance/views.py
@@ -651,7 +651,7 @@ class StripeWebhookView(View):
                 costumer_id = session.get("customer")
                 if subscription_id:
                     models.UserDomain.objects.filter(
-                        id=session.metadata.get("id")
+                        id=session.metadata.get("domain_id")
                     ).update(
                         stripe_subscription_id=subscription_id,
                         stripe_customer_id=costumer_id,
