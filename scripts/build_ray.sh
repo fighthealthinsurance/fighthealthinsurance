f5e81013a154973719b28aad9942690c4ab9df32
Holden Karau <holden@pigscanfly.ca>
2025-03-25 12:35:23 -0700

    Fix deploys to run from totallylegitco instead of holdenk

--- scripts/build_ray.sh
+++ scripts/build_ray.sh
@@ -5,7 +5,7 @@ pwd
 
 # Ray doesn't publish combiend aarch64 & amd64 images because idk.
 RAY_VERSION=2.38.0-py311
-RAY_BASE=${RAY_BASE:-totallylegitco/fhi-ray}
+RAY_BASE=${RAY_BASE:-${MYORG}/fhi-ray}
 RAY_IMAGE=${RAY_BASE}:${RAY_VERSION}
 
 BUILDX_CMD=${BUILDX_CMD:-"push"}
@@ -16,13 +16,13 @@ check_or_build_image() {
 	local ray_version=$2
 	local dockerfile=$3
 
-	docker manifest inspect "${image}" || docker buildx build --platform="${PLATFORM}" -t "${image}" -f "${dockerfile}" "--${BUILDX_CMD}" --build-arg RAY_VERSION="${ray_version}" .
+	docker manifest inspect "${image}" || docker buildx build --platform="${PLATFORM}" -t "${image}" -f "${dockerfile}" "--${BUILDX_CMD}" --build-arg RAY_VERSION="${ray_version}" --build-arg MYORG="${MYORG}" .
 }
 export RAY_VERSION
 
 check_or_build_image "${RAY_IMAGE}" "${RAY_VERSION}" "k8s/ray/RayDockerfile"
 
 # Using the amd64/arm64 ray container as a base put together a container with the FHI code and libs in it.
-COMBINED_IMAGE=holdenk/fhi-ray:${FHI_VERSION}
+COMBINED_IMAGE=${MYORG}/fhi-ray:${FHI_VERSION}
 pwd
 check_or_build_image "${COMBINED_IMAGE}" "${RAY_VERSION}" "k8s/ray/CombinedDockerfile"
