f6f0ae6a21fb8f73cfb154bde748c97edab2de40
Holden Karau <holden@pigscanfly.ca>
2025-05-08 17:55:28 -0700

    coderabbits suggestion was not that good, we don't want to escape []. But we can do the basic escape instead.

--- fighthealthinsurance/prior_auth_utils.py
+++ fighthealthinsurance/prior_auth_utils.py
@@ -53,7 +53,9 @@ class PriorAuthTextSubstituter:
                 if v is None:
                     continue
                 pattern = rf"(?i)(\${k}|\[{k}\])"
-                proposal_text = re.sub(pattern, re.escape(str(v)), proposal_text)
+                # See https://docs.python.org/3/library/re.html#re.escape
+                escaped_value = str(v).replace("\\", r"\\")
+                proposal_text = re.sub(pattern, escaped_value, proposal_text)
             return proposal_text
         except Exception as e:
             logger.error(f"Error substituting values in prior auth text: {e}")
