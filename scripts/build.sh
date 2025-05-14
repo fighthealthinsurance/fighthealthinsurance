8d49a7177e9e734db20b6374b6b3260e7a8d26b5
Holden Karau <holden@pigscanfly.ca>
2025-05-14 12:17:47 -0700

    Deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ BUILDX_CMD=${BUILDX_CMD:-push}
 source "${SCRIPT_DIR}/setup_templates.sh"
 
 # BUILDKIT_NO_CLIENT_TOKEN=true
-FHI_VERSION=v0.12.1c
+FHI_VERSION=v0.12.1d
 
 MYORG=${MYORG:-totallylegitco}
 RAY_BASE=${RAY_BASE:-${MYORG}/fhi-ray}
