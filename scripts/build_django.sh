ff986c7c4d209ae3f15f687d5b8e5348a5a99887
nyghtowl <3422837+nyghtowl@users.noreply.github.com>
2025-07-11 16:26:43 -0700

    Fix the build scripts so blog json is loaded in production.

--- scripts/build_django.sh
+++ scripts/build_django.sh
@@ -8,9 +8,6 @@ PLATFORM=${PLATFORM:-linux/amd64,linux/arm64}
 RELEASE=${FHI_VERSION:-$IMAGE}
 FHI_BASE=${FHI_BASE:-totallylegitco/fhi-base}
 
-# Generate the blog metadata so it's included in the container.
-./manage.py generate_blog_metadata || echo "Warning: Failed to generate blog metadata. Continuing build without it."
-
 # Build the web app
 IMAGE=${FHI_BASE}:${FHI_VERSION}
 (docker manifest inspect "${IMAGE}" && sleep 1) || docker buildx build --platform="${PLATFORM}"  -t "${IMAGE}" -f k8s/Dockerfile --build-arg RELEASE="${RELEASE}" "--${BUILDX_CMD}" .
