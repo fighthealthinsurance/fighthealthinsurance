bae9490e2f503ad7e64f02cd22db7255616a7f5c
Melanie Warrick <3422837+nyghtowl@users.noreply.github.com>
2025-08-14 08:55:15 -0700

    Resolving new package conflicts and issues around django and python. (#471)

--- scripts/setup_templates.sh
+++ scripts/setup_templates.sh
@@ -11,9 +11,14 @@ elif [ -f ./.venv/bin/activate ]; then
 fi
 
 if command -v tox >/dev/null 2>&1; then
-  tox -e mypy
+  # Try interpreter-matched mypy first (py310-mypy/py311-mypy/py312-mypy), then generic, then plain mypy.
+  ENV_NAME="py$(python - <<'PY'
+import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")
+PY
+)-mypy"
+  tox -vv -e "${ENV_NAME}" || tox -vv -e mypy || mypy -p fighthealthinsurance -p fhi_users
 else
-  mypy -p fighthealthinsurance
+  mypy -p fighthealthinsurance -p fhi_users
 fi
 
 ./manage.py migrate
