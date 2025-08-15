0c0b8e9efc18743d636fc44502c41633f72e405d
Melanie Warrick <3422837+nyghtowl@users.noreply.github.com>
2025-08-15 13:54:39 -0700

    Adding robus check if fhi_users doesn't exist for tox and mypy. (#472)

--- scripts/setup_templates.sh
+++ scripts/setup_templates.sh
@@ -11,14 +11,29 @@ elif [ -f ./.venv/bin/activate ]; then
 fi
 
 if command -v tox >/dev/null 2>&1; then
-  # Try interpreter-matched mypy first (py310-mypy/py311-mypy/py312-mypy), then generic, then plain mypy.
-  ENV_NAME="py$(python - <<'PY'
+  # Check if fhi_users directory exists for mypy
+  if [ -d "./fhi_users" ]; then
+    # Try interpreter-matched mypy first (py310-mypy/py311-mypy/py312-mypy), then generic, then plain mypy.
+    ENV_NAME="py$(python - <<'PY'
 import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")
 PY
 )-mypy"
-  tox -vv -e "${ENV_NAME}" || tox -vv -e mypy || mypy -p fighthealthinsurance -p fhi_users
+    tox -vv -e "${ENV_NAME}" || tox -vv -e mypy || mypy -p fighthealthinsurance -p fhi_users
+  else
+    # fhi_users not present, only check fighthealthinsurance
+    ENV_NAME="py$(python - <<'PY'
+import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")
+PY
+)-mypy"
+    tox -vv -e "${ENV_NAME}" || tox -vv -e mypy || mypy -p fighthealthinsurance
+  fi
 else
-  mypy -p fighthealthinsurance -p fhi_users
+  # Direct mypy call - check for fhi_users
+  if [ -d "./fhi_users" ]; then
+    mypy -p fighthealthinsurance -p fhi_users
+  else
+    mypy -p fighthealthinsurance
+  fi
 fi
 
 ./manage.py migrate
