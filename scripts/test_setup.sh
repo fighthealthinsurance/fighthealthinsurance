5103eb4c6e5ea5220912ab75778f5c0b13c0f316
Holden Karau <holden@pigscanfly.ca>
2025-12-25 19:56:22 -0800

    Use test setup script.

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -4,4 +4,4 @@ set -ex
 
 SCRIPT_DIR="$(dirname "$0")"
 
-"${SCRIPT_DIR}/build_static.sh" > logs || (echo "ERROR"; cat logs; exit 1)
+"${SCRIPT_DIR}/"build_static.sh &> logs || (echo "ERROR"; cat logs; exit 1)
